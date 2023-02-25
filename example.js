2514f7cce97ff34e65699d501b4e547fe632852e
sapphire-al2o3 <t_telkun@hotmail.com>
2023-02-25 15:15:52 +0900

    initial commit

---
+++ example.js
@@ -0,0 +1,20 @@
+const CyclicDB = require('@cyclic.sh/dynamodb');
+require('dotenv').config();
+console.log(process.env.CYCLIC_DB);
+console.log(process.env.AWS_ACCESS_KEY_ID);
+const db = CyclicDB(process.env.CYCLIC_DB);
+
+async function run() {
+    let animals = db.collection('animals');
+
+    // create an item in collection with key "leo"
+    let leo = await animals.set('leo', {
+        type:'cat',
+        color:'orange'
+    })
+
+    // // get an item at key "leo" from collection animals
+    // let item = await animals.get('leo');
+    // console.log(item);
+}
+run();
